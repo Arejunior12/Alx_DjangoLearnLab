044d4b0f0806eb7e520925a3f55c9d7b0ee54599
Arejunior12 <arejunior543@gmail.com>
2025-09-06 08:01:22 +0100

    Add files via upload

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,8 @@ from django.views.generic.detail import DetailView
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
-from .models import Book, Library
+from django.contrib.auth.decorators import user_passes_test
+from .models import Book, Library, UserProfile
 
 # Function-based view to list all books
 def list_books(request):
@@ -31,6 +32,45 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
+            login(request, user)
+            return redirect('relationship_app:list_books')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+# Role checking functions
+def is_admin(user):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+# Role-based views
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+
+# Update registration to set default role
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # Set default role to Member
+            user_profile = UserProfile.objects.get(user=user)
+            user_profile.role = 'Member'
+            user_profile.save()
             login(request, user)
             return redirect('relationship_app:list_books')
     else:
