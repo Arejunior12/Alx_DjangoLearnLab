8d91e37fc7d9390cd6e3886cd7945a1316ba3625
Arejunior12 <arejunior543@gmail.com>
2025-09-06 08:23:33 +0100

    Add files via upload

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth.decorators import user_passes_test, permission_required  # CORRECTED IMPORT
+from django.contrib.auth.decorators import permission_required
 from .models import Book, Library, UserProfile, Author
 from .forms import BookForm
 
