69538b81aca5e1ec31ca98c5da9da4ed1f884aec
Arejunior12 <arejunior543@gmail.com>
2025-09-20 10:11:22 +0800

    Add files via upload

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+class BookList(generics.ListAPIView):
+    """API view to list all books"""
+    queryset = Book.objects.all()  # Get all book instances from the database
+    serializer_class = BookSerializer  # Use the BookSerializer to convert to JSON
