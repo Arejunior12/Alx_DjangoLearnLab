8a63e40f4e9638a68ef032d5ccc5217d697ade1e
Arejunior12 <arejunior543@gmail.com>
2025-10-01 21:13:04 +0800

    Add files via upload

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,28 +2,6 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Post, Comment
-# Remove the TagWidget import and use TagField directly
-
-class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-    
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
-    
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.email = self.cleaned_data['email']
-        if commit:
-            user.save()
-        return user
-
-class UserUpdateForm(forms.ModelForm):
-    email = forms.EmailField()
-    
-    class Meta:
-        model = User
-        fields = ['username', 'email']
 
 class PostForm(forms.ModelForm):
     class Meta:
@@ -32,24 +10,8 @@ class PostForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
-            # Remove TagWidget and use a regular TextInput for tags
-            'tags': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter tags separated by commas'}),
+            # Let taggit handle the widget automatically
         }
         help_texts = {
             'tags': 'Enter tags separated by commas',
-        }
-
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        fields = ['content']
-        widgets = {
-            'content': forms.Textarea(attrs={
-                'class': 'form-control',
-                'rows': 3,
-                'placeholder': 'Write your comment here...'
-            }),
-        }
-        labels = {
-            'content': ''
         }
