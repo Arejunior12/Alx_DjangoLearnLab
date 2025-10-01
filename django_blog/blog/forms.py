6a67f6d26f0f49a975e2e8db4cff9b1cfa90a151
Arejunior12 <arejunior543@gmail.com>
2025-10-01 21:19:12 +0800

    Add files via upload

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Post, Comment
+from taggit.forms import TagWidget  # Add this import
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -31,6 +32,7 @@ class PostForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+            'tags': TagWidget(),  # Add this exact line
         }
 
 class CommentForm(forms.ModelForm):
