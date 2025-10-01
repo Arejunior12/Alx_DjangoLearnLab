355e284d26abed78e3e4a40f50a0bea6410fc187
Arejunior12 <arejunior543@gmail.com>
2025-10-01 21:14:51 +0800

    Add files via upload

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -3,6 +3,27 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Post, Comment
 
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
@@ -10,8 +31,23 @@ class PostForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
-            # Let taggit handle the widget automatically
+            'tags': forms.TextInput(attrs={'class': 'form-control'}),
         }
         help_texts = {
             'tags': 'Enter tags separated by commas',
+        }
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': 3,
+                'placeholder': 'Write your comment here...'
+            }),
+        }
+        labels = {
+            'content': ''
         }
